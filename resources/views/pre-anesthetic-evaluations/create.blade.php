fa1b1eef76a574c1641b5dec09388a06e61be7eb
hectordufau <contato@hectordufau.com.br>
2026-05-19 17:06:12 -0300

    cleanup: remove obsolete create/edit views replaced by Livewire modals
    54 views deleted across all 27 Tier 1/Tier 2 modules

--- resources/views/pre-anesthetic-evaluations/create.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.adminlte', ['title' => 'Nova Avaliação Pré-Anestésica'])
-@section('content')
-    <div class="card"><div class="card-body">
-        <form method="POST" action="{{ route('pre-anesthetic-evaluations.store') }}">@csrf
-            @include('pre-anesthetic-evaluations._form')
-            <button type="submit" class="btn btn-primary">Salvar</button>
-        </form>
-    </div></div>
-@endsection
--- resources/views/pre-anesthetic-evaluations/edit.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.adminlte', ['title' => 'Editar Avaliação Pré-Anestésica'])
-@section('content')
-    <div class="card"><div class="card-body">
-        <form method="POST" action="{{ route('pre-anesthetic-evaluations.update', $preAnestheticEvaluation) }}">@csrf @method('PUT')
-            @include('pre-anesthetic-evaluations._form')
-            <button type="submit" class="btn btn-primary">Atualizar</button>
-        </form>
-    </div></div>
-@endsection
