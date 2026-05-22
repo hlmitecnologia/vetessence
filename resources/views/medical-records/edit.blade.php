775dc1aa80e2790704b66da256e044dcc46378f8
hectordufau <contato@hectordufau.com.br>
2026-05-22 09:37:07 -0300

    refactor: bootstrap+branding no formulário de prontuário (livewire)

--- resources/views/medical-records/edit.blade.php
+++ resources/views/medical-records/edit.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.adminlte', ['title' => 'Editar Prontuário'])
 
-@section('header')
-    <a href="{{ route('medical-records.index') }}" class="text-gray-500 hover:text-gray-700"><i class="fas fa-arrow-left"></i></a>
-    <h2 class="ml-4 text-lg font-semibold">Editar Prontuário</h2>
-@endsection
-
 @section('content')
-<div class="max-w-4xl mx-auto">
-    @livewire('medical-record-form', ['recordId' => $medicalRecord->id])
+<div class="row justify-content-center">
+    <div class="col-lg-10">
+        @livewire('medical-record-form', ['recordId' => $medicalRecord->id])
+    </div>
 </div>
 @endsection
