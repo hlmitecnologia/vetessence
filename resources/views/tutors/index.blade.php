a0b1013da4e82bde5247613f2edfb45195cf7574
hectordufau <contato@hectordufau.com.br>
2026-06-18 17:06:40 -0300

    fix: remove pagination links (Datatables ja gerencia)

--- resources/views/tutors/index.blade.php
+++ resources/views/tutors/index.blade.php
@@ -60,9 +60,6 @@
                 @endforeach
             </tbody>
         </table>
-        <div class="mt-3">
-            {{ $tutors->links() }}
-        </div>
         @else
         <p class="text-center text-muted">Nenhum registro encontrado.</p>
         @endif
