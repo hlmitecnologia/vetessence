d743e31f3976de77ce6563b02d2c450240b5af2c
hectordufau <contato@hectordufau.com.br>
2026-07-28 09:36:25 -0300

    fix: add screen_time PT5M to MP order payload for terminal display

--- app/Services/Payment/Providers/MercadoPagoProvider.php
+++ app/Services/Payment/Providers/MercadoPagoProvider.php
@@ -238,6 +238,7 @@ class MercadoPagoProvider implements PaymentGatewayProvider
                     'point' => [
                         'terminal_id' => $terminalId,
                         'print_on_terminal' => 'no_ticket',
+                        'screen_time' => 'PT5M',
                     ],
                     'payment_method' => [
                         'default_type' => 'credit_card',
